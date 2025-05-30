e398390956b0ad76f750ffcc9ca0d6b99d12e3ff
Reyka <blueorange477@gmail.com>
2025-05-30 15:49:09 +0300

    Update coin logic

--- Scripts/Item/Coin.cs
+++ Scripts/Item/Coin.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 using TMPro;
 public class Coin : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI coinText;
     [SerializeField] private string pickupTriggerName = "Pickup";
+    [SerializeField] private AudioSource audioSource;
+    
     public PlayerInfo PlayerInfo;
     private bool collected = false;
     private Animator anim;
@@ -11,6 +12,8 @@ public class Coin : MonoBehaviour
     private void Awake()
     {
         anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        PlayerInfo = FindObjectOfType<PlayerInfo>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -21,6 +24,10 @@ public class Coin : MonoBehaviour
         // Добавляем монеты, обновляем текст
         collected = true;
         PlayerInfo.UpdateCurrencyText("coin");
+        
+        // Запускаем звук
+        if (audioSource != null)
+            audioSource.Play();
 
         // Запускаем анимацию
         if (anim != null)
